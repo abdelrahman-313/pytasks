740e6734747072fe9c9a70dffc62e454eae621a0
asamir <asamir@ciena.com>
2021-08-02 01:03:51 +0200

    task2

---
+++ task2/remove_vowels.py
@@ -0,0 +1,9 @@
+def rem_vowel(word):
+    vowels = ['a','e','i','o','u']
+    result = [l for l in word if l.lower() not in vowels]
+    result = ''.join(result)
+    print(result)
+
+    
+if __name__ == '__main__':
+    rem_vowel('Mobile')
